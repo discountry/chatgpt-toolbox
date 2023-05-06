01e959fc19a46ff0091d8eedbf235fe13ca9391f
disney <disney@tokeninsight.com>
2023-05-06 23:30:50 +0800

    change icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,10 @@ import "./globals.css";
 export const metadata = {
   title: "ChatGPT Toolbox",
   description: "The ChatGPT that can store your prompts.",
+  appleWebApp: {
+    title: "ChatGPT Toolbox",
+    statusBarStyle: "black-translucent",
+  },
 };
 
 export default function RootLayout({
