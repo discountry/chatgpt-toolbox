201a1aec2826f99b974da2ac4f4cc7272fee32b7
Disney <4507101+discountry@users.noreply.github.com>
2023-11-14 19:25:13 +0800

    Update config.ts

--- utils/config.ts
+++ utils/config.ts
@@ -1,3 +1,3 @@
 export const OPENAI_URL = `https://openai.yubolun.com/v1/chat/completions`;
-export const OPENAI_MODEL = "gpt-4";
+export const OPENAI_MODEL = "gpt-3.5-turbo-16k";
 export const MAX_TOKENS = 16384;
