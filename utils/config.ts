ccf565c189f971ba8dc13d0c20042764e14cb03e
Disney <4507101+discountry@users.noreply.github.com>
2024-02-04 10:44:45 +0800

    Update config.ts

--- utils/config.ts
+++ utils/config.ts
@@ -1,3 +1,3 @@
-export const OPENAI_URL = `https://openai.yubolun.com/v1/chat/completions`;
+export const OPENAI_URL = `https://api.openai.com/v1/chat/completions`;
 export const OPENAI_MODEL = "gpt-4-turbo-preview";
 export const MAX_TOKENS = 16384;
